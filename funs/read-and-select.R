c0e324e0a3cb506ffca1a6dded6eeee07f36ccad
Sebastian Sauer <ssauer@posteo.de>
2022-12-13 22:50:19 +0100

    untrack large files

--- funs/read-and-select.R
+++ funs/read-and-select.R
@@ -1,7 +1,7 @@
-read_and_select <- function(file_name, path_to_tweet_data = tweet_data_path) {
+read_and_select <- function(file_name) {
   
   out <- 
-    read_rds(file = paste0(path_to_tweet_data, file_name)) %>% 
+    read_rds(file = file_name) %>% 
     select(id, author_id, created_at, text, public_metrics) %>% 
     unnest_wider(public_metrics)
   
