e133f06b6c2ff098634a9d7f2d6d71c70fead281
Sebastian Sauer <ssauer@posteo.de>
2023-10-22 22:24:15 +0200

    entropy in own chapter

--- funs/helper-funs-recipes.R
+++ funs/helper-funs-recipes.R
@@ -1,24 +1,24 @@
 
 
 count_profane <- function(text, profane_list = schimpf$word) {
-  sum((tokenize_tweets(text, simplify = TRUE) %>% simplify()) %in% profane_list)
+  sum((tokenize_words(text, simplify = TRUE) %>% simplify()) %in% profane_list)
 }
 
 
 
 count_emo_words <- function(text, emo_list = sentiws$word) {
-  sum((tokenize_tweets(text, simplify = TRUE) %>% simplify()) %in% emo_list)
+  sum((tokenize_words(text, simplify = TRUE) %>% simplify()) %in% emo_list)
 }
 
 
 count_emojis <- function(text, emoji_list = trimws(emj)){
-  sum((tokenize_tweets(text, simplify = TRUE) %>% simplify()) %in% emoji_list)
+  sum((tokenize_words(text, simplify = TRUE) %>% simplify()) %in% emoji_list)
 }
 
 
 
 count_wild_emojis <- function(text, wild_emoji_list = wild_emojis){
-  sum((tokenize_tweets(text, simplify = TRUE) %>% simplify()) %in% wild_emoji_list)
+  sum((tokenize_words(text, simplify = TRUE) %>% simplify()) %in% wild_emoji_list)
 }
 
 
